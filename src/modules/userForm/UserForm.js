e9ca3d011c9f47d7f660d20f6d68c77f59c96b3f
Daniel Teófilo <danndevfull@gmail.com>
2021-09-01 16:25:56 -0500

    feat: implementing form user

--- src/modules/userForm/UserForm.js
+++ src/modules/userForm/UserForm.js
@@ -1,28 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const UserForm = () => {
+  const [data, setData] = useState({});
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    const dataForm = { ...data };
+    dataForm[name] = value;
+    setData(dataForm);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.setItem("user", JSON.stringify(data));
+    console.log("all data:", e, data);
+  };
+
+  const { name, lastName, phone, email } = data;
+
   return (
-    <div>
-      <div>Welcome</div>
-      <form action="">
-        <div>
-          <label htmlFor="">Name</label>
-          <input type="text" name="" />
-        </div>
-        <div>
-          <label htmlFor="">Last Name</label>
-          <input type="text" name="" />
-        </div>
-        <div>
-          <label htmlFor="">email</label>
-          <input type="email" name="" />
-        </div>
-        <div>
-          <label htmlFor="">Phone Number</label>
-          <input type="text" name="" />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+    <div className="container">
+      <div className="card">
+        <div>Welcome</div>
+        <form onSubmit={handleSubmit} autoComplete="off">
+          <div>
+            <label htmlFor="" className="label-form">
+              Name
+            </label>
+            <input
+              required="true"
+              type="text"
+              value={name}
+              name="name"
+              className="form-control"
+              onChange={(event) => handleChange(event)}
+            />
+          </div>
+          <div>
+            <label htmlFor="" className="label-form">
+              Last Name
+            </label>
+            <input
+              type="text"
+              value={lastName}
+              name="lastName"
+              className="form-control"
+              onChange={(event) => handleChange(event)}
+            />
+          </div>
+          <div>
+            <label htmlFor="" className="label-form">
+              email
+            </label>
+            <input
+              type="email"
+              required="true"
+              value={email}
+              name="email"
+              className="form-control"
+              onChange={(event) => handleChange(event)}
+            />
+          </div>
+          <div>
+            <label htmlFor="" className="label-form">
+              Phone Number
+            </label>
+            <input
+              type="text"
+              name="phone"
+              value={phone}
+              className="form-control"
+              onChange={(event) => handleChange(event)}
+            />
+          </div>
+          <button type="submit" className="btn">
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
